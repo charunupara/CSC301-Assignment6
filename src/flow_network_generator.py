1795b85a14e382ddbb75a754c5d3c57e41cc5eb5
charunpara <charunupara1@gmail.com>
2019-12-04 20:44:48 -0600

    Finish generator

--- src/flow_network_generator.py
+++ src/flow_network_generator.py
@@ -2,94 +2,78 @@
 import random
 import networkx as nx
 import matplotlib.pyplot as plt
+import pydot
+from networkx.drawing.nx_pydot import write_dot
 
-order = random.randint(4,6)
+# maximum capacity of the flow network
+max_capacity = 20
 
-G = nx.DiGraph()
+def create_flow(id):
 
-for i in range(1, order+1):
-    G.add_node(i)
-
-
-source = 1
-sink = order + 1
-# G.add_edge(source, sink)
+    # random number of nodes
+    order = random.randint(4,6)
 
 
-outgoing_source_edges = random.randint(2, order - 1)
-print(outgoing_source_edges)
+    G = nx.DiGraph()
 
-node_dict = {}
-nodeList = list(G.nodes)
-print(nodeList[0], nodeList[-1])
-# create outgoing edges from source to random vertices
-for i in range(0, outgoing_source_edges):
-    G.add_edge(nodeList[0], nodeList[i])
+    for i in range(1, order+1):
+        G.add_node(i)
 
-for i in range(1, len(nodeList)-1):
-    for j in range(i, len(nodeList)):
-        curr_node = nodeList[i]
-        noti = random.randint(1, len(nodeList)-1)
-        while noti == i:
-            noti = random.randint(1, len(nodeList)-1)
-        random_node = nodeList[noti]
-        valid_edge = (curr_node, random_node)
-        while valid_edge in node_dict.keys() or valid_edge[::-1] in node_dict.keys():
+
+    source = 1
+    sink = order + 1
+    # G.add_edge(source, sink)
+
+
+    outgoing_source_edges = random.randint(2, order - 1)
+    
+
+    node_dict = {}
+    nodeList = list(G.nodes)
+   
+    # create outgoing edges from source to random vertices
+    for i in range(1, outgoing_source_edges):
+        G.add_edge(nodeList[0], nodeList[i], capacity=random.randint(1, max_capacity), flow=0)
+        node_dict[(nodeList[0], nodeList[i])] = 0
+
+    for i in range(1, len(nodeList)-1):
+        for j in range(i, len(nodeList)):
+            curr_node = nodeList[i]
             noti = random.randint(1, len(nodeList)-1)
             while noti == i:
                 noti = random.randint(1, len(nodeList)-1)
             random_node = nodeList[noti]
             valid_edge = (curr_node, random_node)
-            break
-            print(valid_edge, node_dict)
-
-        G.add_edge(curr_node, random_node)
-        node_dict[valid_edge] = 0
-
-# size = random.randint(order - 1, (order * (order - 1)))
-
-# '''
-# Start with source
-# Pick random number of edges
-
-# '''
-
-
-# print(nodeList)
-# for i in range(0, size + 1):
+            while valid_edge in node_dict.keys() or valid_edge[::-1] in node_dict.keys():
+                noti = random.randint(1, len(nodeList)-1)
+                while noti == i:
+                    noti = random.randint(1, len(nodeList)-1)
+                random_node = nodeList[noti]
+                valid_edge = (curr_node, random_node)
+                break
+                
+            # Remove self-loop and antiparallel edges
+            rev_tuple = (random_node, curr_node)
+            if rev_tuple not in node_dict and rev_tuple[0] != rev_tuple[1] :
+                G.add_edge(curr_node, random_node, capacity=random.randint(1, max_capacity), flow=0)
+                node_dict[valid_edge] = 0
+            
+                    
 
 
-#     random_source_index = random.randint(1, len(nodeList) - 1)
-#     random_dest_index = random.randint(1, len(nodeList) - 1)
+    pos = nx.random_layout(G)
 
-#     while random_source_index == random_dest_index:
-#         random_dest_index = random.randint(1, len(nodeList) - 1)
-    
-#     cur = (random_source_index, random_dest_index)
+    labels = nx.get_edge_attributes(G,'capacity')
+    nx.draw(G, pos=pos, with_labels=True, font_weight='bold')
+    nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
 
-
-#     if cur not in node_dict:
-#         G.add_edge(random_source_index, random_dest_index)
-#         node_dict[cur] = 0
-#     else:
-#         random_source_index = random.randint(1, len(nodeList) - 1)
-#         random_dest_index = random.randint(1, len(nodeList) - 1)
-
-#         while random_source_index == random_dest_index:
-#             random_dest_index = random.randint(1, len(nodeList) - 1)
-
-#         cur = (random_source_index, random_dest_index)
-
-#         G.add_edge(random_source_index, random_dest_index)
-#         node_dict[cur] = 0
-        
-        
+    plt.savefig(r"C:\Users\Charun\Desktop\CSC301-Assignment6\input_graphs\input_" + str(id) + r".png")
+    write_dot(G, r"C:\Users\Charun\Desktop\CSC301-Assignment6\input_graphs\input_" + str(id) + r".dot")
+    plt.close()
+    G.clear()
 
 
-pos = nx.random_layout(G)
 
-nx.draw(G, pos=pos, with_labels=True, font_weight='bold')
 
-print(node_dict)
-plt.show()
-
+for i in range(1, 11):
+    create_flow(i)
--- src/max_flow_generator.py
+++ src/max_flow_generator.py
@@ -1,2 +1,65 @@
-import flow_network_generator
+import random
+import networkx as nx
+import matplotlib.pyplot as plt
+import pydot
+from networkx.drawing.nx_pydot import read_dot
+from collections import deque
+from tryalgo.graph import add_reverse_arcs
 
+graph = read_dot(
+    r"C:\Users\Charun\Desktop\CSC301-Assignment6\input_graphs\input_1.dot")
+
+
+def augment(graph, flow, source, target):
+    n = len(graph)
+    A = [0] * n
+    augm_path = [None] * n
+    Q = deque()
+    Q.append(source)
+    augm_path[source] = source
+    A[source] = float('inf')
+    while Q:
+        u = Q.popleft()
+        for v in graph.neighbors(str(u)):
+           cuv = graph[str(u)][str(v)][0]['capacity']
+           residual = cuv - graph[str(u)][str(v)][0]['flow']
+           if residual > 0 and augm_path[v] is None:
+                augm_path[v] = u
+                A[v] = min(A[u], residual)
+                if v == target:
+                   break
+                else:
+                    Q.append(v)
+
+    return (augm_path, A[target])          
+
+def edmonds_karp(graph, capacity, source, target):
+    """Maxmum flow by Edmonds-Karp
+
+    :param graph: directed graph in listlist or listdict format
+    :param capacity: in matrix format or same listdict graph
+    :param int source: vertex
+    :param int target: vertex
+    :returns: flow matrix, flow value
+    :complexity: :math:`O(|V|*|E|^2)`
+    """
+    add_reverse_arcs(graph, capacity)
+    V = range(len(list((graph.nodes()))))
+    flow = [[0 for v in V] for u in V]
+    while True:
+        augm_path, delta = augment(graph, capacity, flow, source, target)
+        if delta == 0:
+            break
+        v = target                    # remonter vers la source
+        while v != source:
+            u = augm_path[v]          # et augmenter le flot
+            flow[u][v] += delta
+            flow[v][u] -= delta
+            v = u
+    return (flow, sum(flow[source]))  # flot, valeur du flot
+# snip}
+
+
+
+# augment(graph, 1, 1,2)
+print(edmonds_karp(graph, )
